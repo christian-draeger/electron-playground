2556aecdaa44c1cf7adcc833d5065f877f0763f7
Christian Dräger <christian.draeger1@gmail.com>
2018-08-30 13:00:28 +0200

    can open main window

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const electron = require('electron');
 
-const { app } = electron;
+const { app, BrowserWindow } = electron;
 
 app.on('ready', () => {
-    console.log('app is now ready!!!')
+    new BrowserWindow({});
 });
