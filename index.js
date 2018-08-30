89d4547a71c24a30d1a8209b3c578c66ad6cdec2
Christian Dräger <christian.draeger1@gmail.com>
2018-08-30 14:31:14 +0200

    can send event via IPC form browser to electron app

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+const {app, BrowserWindow, ipcMain} = electron;
+
+let mainWindow;
 
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`)
+});
+
+ipcMain.on('video:submit', (event, path) => {
+    console.log(path);
 });
